1c541475b5f12ba27860bc250ff6618623eecbfb
Prathamesh Shinde <prathameshshinde1052001@gmail.com>
2025-03-08 21:02:45 +0530

    update

--- resumebuilder/model/Summary.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SummarySchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  text: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Summary', SummarySchema);
